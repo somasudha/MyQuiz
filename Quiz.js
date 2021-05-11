df561b02f7713e518af1ed32cd0319c8e1fb7ac1
somasudha <somasudha@gmail.com>
2021-05-10 18:22:25 -0700

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,54 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    Question.title.hide();
+    Question.input1.hide();
+    Question.button.hide();
+    Question.input2.hide();
+
+    text("Result:", 425, 350);
+
+    if (allContestants !== undefied) {
+      fill("Blue");
+      textSize(20);
+      text("If your name is green, you have the right answer!", 130, 230);
+    }
+
+    for(var plr in allContestants){
+      debugger;
+      var correctAns = "2";
+      if (correctAns === allContestants[plr].answer)
+        fill("Green")
+      else
+        fill("red");    
+  }
+
+}
