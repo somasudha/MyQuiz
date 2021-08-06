f95712a05ec3982b7e62baa0fa866726cf626a56
somasudha <somasudha@gmail.com>
2021-08-06 09:45:48 -0700

    Update Question.js

--- Question.js
+++ Question.js
@@ -20,19 +20,38 @@ class Question {
   }
 
   display(){
-    this.title.html("Riddle");
+    this.title.html("MyQuiz Game");
     this.title.position(350, 0);
 
-    this.question.html("Question:- You have to go into one of these three rooms for 30 minutes. Which one is the safest? " );
+    this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
     this.question.position(150, 80);
-    this.option1.html("1: The room with raging fires. " );
+    this.option1.html("1: Everyone " );
     this.option1.position(150, 100);
-    this.option2.html("2: The room with hungry lions who haven't eaten in a long time." );
+    this.option2.html("2: Envelope" );
     this.option2.position(150, 120);
-    this.option3.html("3: The room filled with balls and doesn't let you breathe." );
+    this.option3.html("3: Estimate " );
     this.option3.position(150, 140);
-    this.option4.html("4: I don't know." );
+    this.option4.html("4: Example" );
     this.option4.position(150, 160);
+
+    this.input1.position(150, 230);
+    this.input2.position(350, 230);
+    this.button.position(290, 300);
+
+    this.button.mousePressed(()=>{
+      this.title.hide();
+      this.input1.hide();
+      this.input2.hide();
+      this.button.hide();
+      contestant.name = this.input1.value();
+      contestant.answer = this.input2.value();
+      contestantCount+=1;
+      contestant.index = contestantCount;
+      contestant.update();
+      contestant.updateCount(contestantCount);
+    });
+  }
+}
 
     this.input1.position(150, 230);
     this.input2.position(350, 230);
